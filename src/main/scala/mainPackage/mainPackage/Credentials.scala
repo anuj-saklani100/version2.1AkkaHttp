4fb0e5341e21de66717d92e9c41c2020e133d997
anuj-saklani100 <131871460+anuj-saklani100@users.noreply.github.com>
2023-05-23 13:57:33 +0530

    refactor1

--- src/main/scala/mainPackage/mainPackage/Credentials.scala
+++
@@ -1,7 +0,0 @@
-package mainPackage
-
-object Credentials {
-  val url = "jdbc:mysql://localhost:3306/megaBase"
-  val username = "root"
-  val password = "Tellius@12345"
-}
--- src/main/scala/mainPackage/mainPackage/allHandlers/ConnectorCon.scala
+++
@@ -1,9 +0,0 @@
-package mainPackage.allHandlers
-import mainPackage.Credentials.{password, url, username}
-
-import java.sql.{Connection, DriverManager}
-object ConnectorCon {
-  def getConnection(): Connection = {
-    DriverManager.getConnection(url, username, password)
-  }
-}
