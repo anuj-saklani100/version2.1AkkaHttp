d14b57c80e9a05c12bb3db4a1f97343141bbb4c2
anuj-saklani100 <131871460+anuj-saklani100@users.noreply.github.com>
2023-05-23 15:02:03 +0530

    RefactorCommit

---
+++ src/main/scala/mainpackage/Validations.scala
@@ -0,0 +1,25 @@
+package mainpackage
+
+import scala.util.matching.Regex
+
+
+
+trait ValidationTrait {
+  // ------------------------- password validation mechanism --------------------
+  def isValidate(pass: String):Boolean
+}
+
+object Validations extends ValidationTrait{
+ override  def isValidate(pass: String): Boolean = {
+    val minLength = 8
+    val hasDigitRegex: Regex = ".*\\d.*".r
+    val hasCharRegex: Regex = ".*[a-zA-Z].*".r
+    val hasSpecialCharRegex: Regex = ".*[^a-zA-Z0-9].*".r
+
+    (pass.length >= minLength) &
+      (hasDigitRegex.findFirstMatchIn(pass).isDefined) &
+      (hasCharRegex.findFirstMatchIn(pass).isDefined) &
+      (hasSpecialCharRegex.findFirstMatchIn(pass).isDefined)
+  }
+}
+
